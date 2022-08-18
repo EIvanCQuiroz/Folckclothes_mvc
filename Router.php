f9b92536f552efeda8ec76baa238f1f3bd60bb57
Edgar Ivan Quiroz Calderon <ingenieroquiroz03@gmail.com>
2022-08-18 10:03:01 -0500

    "al original"

--- Router.php
+++ Router.php
@@ -25,14 +25,15 @@ class Router
 
         // Arreglo de rutas protegidas...
         // $rutas_protegidas = ['/admin', '/propiedades/crear', '/propiedades/actualizar', '/propiedades/eliminar', '/vendedores/crear', '/vendedores/actualizar', '/vendedores/eliminar'];
-        //$auth = $_SESSION['login'] ?? null;
-
+        $auth = $_SESSION['login'] ?? null;
+/*
         if ($_SERVER['PATH_INFO']) {
             $currentUrl = $_SERVER['PATH_INFO'] ?? '/';
          } else {
             $currentUrl = $_SERVER['REQUEST_URI'] === '' ? '/' : $_SERVER['REQUEST_URI'];
          }
-        /*$currentUrl = ($_SERVER['REQUEST_URI'] === '') ? '/' :  $_SERVER['REQUEST_URI'] ;
+         */
+        $currentUrl = ($_SERVER['REQUEST_URI'] === '') ? '/' :  $_SERVER['REQUEST_URI'] ;
         $method = $_SERVER['REQUEST_METHOD'];
     
         //dividimos la URL actual cada vez que exista un '?' eso indica que se están pasando variables por la url
@@ -43,7 +44,7 @@ class Router
         $fn = $this->getRoutes[$splitURL[0]] ?? null; //$splitURL[0] contiene la URL sin variables 
         } else {
         $fn = $this->postRoutes[$splitURL[0]] ?? null;
-        }  */ 
+        }  
 
         /*$currentUrl = strtok($_SERVER["REQUEST_URI"], '?') ?? '/';
         $method = $_SERVER['REQUEST_METHOD'];
